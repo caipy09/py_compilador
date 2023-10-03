010c7610c5bb6165195b818f67122f0bdedcbbd4
FernandoJesus21 <fernando17000@outlook.com>
2023-10-03 15:56:14 -0300

    pendiente: resolver 2 shift-reduce
    con el ejemplo de text.txt la salida es afectada por dos conflictos shift-reduce, que desaparece eliminando la regla Rule 8     expression -> RESTA expression. No obstante, esta regla es necesaria para aceptar valores negativos. Queda pendiente ver si es viable redefinir la regla para evitar ambiguedades.

--- test_parser.py
+++ test_parser.py
@@ -1,14 +1,25 @@
 import lex as lex
 
-tokens = (
-    "ID", "CTE", "SUMA", "RESTA", "MULT", "DIV", "PARA", "PARC", "ASIG"
-)
-
+#orden de precedencia de operadores
 precedence = (
     ('left', 'SUMA', 'RESTA'),
     ('left', 'MULT', 'DIV'),
     ('right', 'UMINUS'),
 )
+
+#lista de palabras reservadas
+reserved = {
+   'if' : 'IF',
+   'else' : 'ELSE',
+   'while' : 'WHILE',
+   'print' : 'PRINT',
+   'int' : 'INT'
+}
+
+#lista completa de tokens
+tokens = [
+    "ID", "CTE", "SUMA", "RESTA", "MULT", "DIV", "PARA", "PARC", "ASIG"
+] + list(reserved.values())
 
 
 #x = lex.Lexer(filename='C:/Users/ferna/Onedrive/Documents/GitHub/py_compilador/test.txt')
@@ -59,19 +70,6 @@ def p_expression_uminus(p):
     "expression : RESTA expression %prec UMINUS"
     p[0] = -p[2]
 
-#regla que permite el uso de constantes con un más adelante (+1) al inicio de una expresion    
-def p_expression_uplus(p):
-    "expression : SUMA expression"
-    p[0] = p[2]
-
-# def p_expression_id(p):
-#     "expression : ID"
-#     try:
-#         p[0] = names[p[1]]
-#     except LookupError:
-#         print("Undefined id '%s'" % p[1])
-#         p[0] = 0
-
 #para la multiplicacion de variables/constantes (2 * 3)
 def p_term_mult(p):
     'term : term MULT factor'
@@ -106,6 +104,23 @@ def p_factor_expr(p):
     'factor : PARA expression PARC'
     p[0] = p[2]
 
+# def p_empty(p):
+#     'empty :'
+#     pass
+
+
+#regla que permite el uso de constantes con un más adelante (+1) al inicio de una expresion    
+# def p_expression_uplus(p):
+#     "expression : SUMA expression"
+#     p[0] = p[2]
+
+# def p_expression_id(p):
+#     "expression : ID"
+#     try:
+#         p[0] = names[p[1]]
+#     except LookupError:
+#         print("Undefined id '%s'" % p[1])
+#         p[0] = 0
 
 # Error rule for syntax errors
 def p_error(p):
@@ -115,8 +130,8 @@ def p_error(p):
         print("Syntax error at EOF")
 
 # Build the parser
-parser = yacc.yacc()
-parser = yacc.parse(lexer=x, debug=True)
+yacc.yacc(write_tables=False)
+yacc.parse(lexer=x, debug=True)
 
 
 
