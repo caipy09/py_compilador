d80077ec40f2f81736be943ffcdf666fc4df376c
FernandoJesus21 <fernando17000@outlook.com>
2023-10-16 00:04:45 -0300

    Agregadas reglas faltantes
    Estan presentes las reglas if while else.
    Ahora se guarda la ts, polaca y errores/warnings en un txt

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 
+ntest = "00"
+path_input = "C:/Users/ferna/Onedrive/Documents/GitHub/py_compilador/lote_pruebas/" + ntest + "/"
+path_output = "C:/Users/ferna/Onedrive/Documents/GitHub/py_compilador/lote_pruebas/" + ntest + "/"
+
+
 ############################################
 #Codigo para probar el funcionamiento del lexico y sintactico
 ############################################
 
 #importo codigo de la ts y el lexer
 import lexer as lex
-#creo el objeto lexer
-lexer = lex.Lexer(filename='C:/Users/ferna/Onedrive/Documents/GitHub/py_compilador/test1.txt')
+#creo el objeto lexer 
+lexer = lex.Lexer(filename= path_input + 'input.txt')
 
 ###############################################################
 #prueba del sintactico
@@ -18,14 +23,14 @@ import ply.yacc as yacc
 precedence = (
     ('left', 'SUMA', 'RESTA'),
     ('left', 'MULT', 'DIV'),
-    ('right', 'URESTA'), #contempla resta unaria
+    #('right', 'URESTA'), #contempla resta unaria, si las reglas de resta unaria estan comentadas comentar esto tambien
 )
 
 #lista de palabras reservadas
 reserved = {
-   #'if' : 'IF',
-   #'else' : 'ELSE',
-   #'while' : 'WHILE',
+   'if' : 'IF',
+   'else' : 'ELSE',
+   'while' : 'WHILE',
    'print' : 'PRINT',
    'int' : 'INT'
 }
@@ -54,7 +59,7 @@ tokens = [
 ] + list(reserved.values())
 
 
-
+err = []
 polaca_inversa = []
 
 #########################################################################################
@@ -85,6 +90,16 @@ def p_logic_statement(p):
     'statement : logic_statement'
     pass
 
+#una declaracion puede una declaracion de un while
+def p_while_statement(p):
+    'statement : while_statement'
+    pass
+
+#una declaracion puede una declaracion de un if
+def p_if_statement(p):
+    'statement : if_statement'
+    pass
+
 #bloque de declaraciones
 def p_statement_decl(p):
     'statement : LLAVEA INT block_decl'
@@ -110,9 +125,14 @@ def p_statement_asig(p):
     'statement : ID ASIG expression'
     if lexer.ts.getDeclaration(p[1]): 
         lexer.ts.setSymbolValue(p[1], p[3])
-        lexer.ts.setSymbolLength(p[1], len(str(p[3])))     
+        lexer.ts.setSymbolLength(p[1], len(str(p[3]))) 
+        #generacion de polaca inversa
+        polaca_inversa.append(p[1])
+        polaca_inversa.append(p[2])        
     else:
-        print("[ERR] Variable no declarada: ", p[1])
+        e = "[ERR] Variable no declarada: ", p[1]
+        err.append(e)
+        print(e)
         raise(SyntaxError)
 
 #una expresion puede ser un termino
@@ -124,11 +144,15 @@ def p_expression(p):
 def p_expression_suma(p):
     'expression : expression SUMA term'
     p[0] = int(p[1]) + int(p[3])
+    #generacion de polaca inversa
+    polaca_inversa.append("+")
 
 #una termino puede restarse de una expresion
 def p_expression_resta(p):
     'expression : expression RESTA term'
     p[0] = int(p[1]) - int(p[3])
+    #generacion de polaca inversa
+    polaca_inversa.append("-")
 
 #un termino es un factor
 def p_term(p):
@@ -139,6 +163,8 @@ def p_term(p):
 def p_expression_mult(p):
     'term : term MULT factor'
     p[0] = int(p[1]) * int(p[3])
+    #generacion de polaca inversa
+    polaca_inversa.append("*")
 
 #un factor puede dividir a un termino, si la division arroja un float, castea el float a int y tira un warning avisando    
 def p_expression_div(p):
@@ -148,7 +174,11 @@ def p_expression_div(p):
         p[0] = aux
     else:
         p[0] = int(aux)
-        print("[WAR] DIV result was casted to int: ", aux , " >> ", p[0])
+        e = "[WAR] DIV result was casted to int: " + str(aux) + " >> " + str(p[0])
+        print(e)
+        err.append(e)
+    #generacion de polaca inversa
+    polaca_inversa.append("/")
 
 #un factor puede ser una expresion encerrada entre parentesis
 def p_factor_par(p):
@@ -159,22 +189,29 @@ def p_factor_par(p):
 def p_factor_1(p):
     'factor : CTE'
     p[0] = int(p[1])
+    #generacion de polaca inversa
+    polaca_inversa.append(p[1])
 
 #un factor puede ser un identificador, en este caso busca el valor numerico del identificador antes de asignar a p
 def p_factor_2(p):
     'factor : ID'
     valorSimbolo = (lexer.ts.getSymbolByID(p[1])["value"]).iloc[0]
     p[0] = valorSimbolo
+    #generacion de polaca inversa
+    polaca_inversa.append(p[1])
 
-#permite restas unarias 
-def p_factor_cte_n(p):
-    "factor : RESTA CTE %prec URESTA"
-    p[0] = -int(p[2])
-
-#permite expresiones como por ej: -(-5 * 8)
-def p_factor_expr_n(p):
-    'factor : RESTA PARA expression PARC %prec URESTA'
-    p[0] = -int(p[3])
+# =============================================================================
+# #estas reglas generan 1 shift-reduce, sin embargo permiten operaciones unarias con constantes y expresiones
+# #permite restas unarias, ej: x = -5
+# def p_factor_cte_n(p):
+#     "factor : RESTA CTE %prec URESTA"
+#     p[0] = -int(p[2])
+# 
+# #permite expresiones como por ej: -(-5 * 8)
+# def p_factor_expr_n(p):
+#     'factor : RESTA PARA expression PARC %prec URESTA'
+#     p[0] = -int(p[3])
+# =============================================================================
 
 #imprimir valor
 def p_print(p):
@@ -196,6 +233,7 @@ def p_comparator_dist(p):
 def p_comparator_menor(p):
     'comp_statement : expression MENOR factor'
     p[0] = p[1] < p[3]
+    #print(p[0], " : ", p[1], " MENOR ", p[3])
     
 #comparador <=
 def p_comparator_menorigual(p):
@@ -216,125 +254,61 @@ def p_comparator_mayorigual(p):
 def p_logic_expr_or(p):
     'logic_statement : comp_statement OR comp_expression'
     p[0] = p[1] or p[3]
-    print(p[0], " : ", p[1], " OR ", p[3])
+    #print(p[0], " : ", p[1], " OR ", p[3])
 
 #el operador logico AND puede ser usado en dos expresiones de comparacion
 def p_logic_expr_and(p):
     'logic_statement : comp_statement AND comp_expression'
     p[0] = p[1] and p[3]
-    print(p[0], " : ", p[1], " AND ", p[3])
+    #print(p[0], " : ", p[1], " AND ", p[3])
 
 #una expresion de comparacion es una declaracion de comparacion
 def p_comp_expression(p):
     'comp_expression : comp_statement'
     p[0] = p[1]
 
+#preguntar si los while/if deben ejecutarse en etapa de parsing
+#preguntar de no ejecutarse en esta etapa, que hacer para mantener actualizada las TS
+#https://stackoverflow.com/questions/62603001/while-cycle-implementation-never-ends-in-ply-issue
+#https://copyprogramming.com/howto/python-ply-issue-with-if-else-and-while-statements#python-ply-issue-with-if-else-and-while-statements
 
+#regla del while
+def p_while(p):
+    '''while_statement : WHILE PARA comp_statement PARC LLAVEA sub_program
+                       | WHILE PARA logic_statement PARC LLAVEA sub_program'''
+    #print("p_while")
 
+#regla del bloque de codigo dentro de una sentencia de control
+def p_subprogram(p):
+    'sub_program : program LLAVEC'
 
+#regla del if
+def p_condition_if(p):
+    '''if_statement : IF PARA comp_statement PARC LLAVEA sub_program else_statement
+                    | IF PARA logic_statement PARC LLAVEA sub_program else_statement'''
+    #print("p_condition_if")    
 
+#regla del else
+def p_condition_else(p):
+    '''else_statement : ELSE LLAVEA sub_program
+                      | empty'''
+    #print("p_condition_if_else")
 
-
-# #cada declaracion se puede componer de una expresion
-# def p_statement_expr(p):
-#     'statement : expression'
-#     pass
-
-# #para la asignacion de valores a variables (x = 2)
-# def p_expression_asig(p):
-#     'statement : ID ASIG expression'
-#     lexer.ts.setSymbolValue(p[1], p[3])
-#     lexer.ts.addSymbol(str(int(p[3])), str(int(p[3])), str(int(p[3])), len(str(int(p[3]))), None) #agrega constante calculada
-#     lexer.ts.setSymbolLength(p[1], len(str(p[3])))
-#     #generacion de polaca inversa
-#     polaca_inversa.append(p[1])
-#     polaca_inversa.append(p[2])
-
-# #para la suma de variables/constantes (2 + 3)
-# def p_expression_suma(p):
-#     'expression : expression SUMA term'
-#     p[0] = int(p[1]) + int(p[3])
-#     #generacion de polaca inversa
-#     polaca_inversa.append("+")
-
-# #para la resta de variables/constantes (2 - 3)
-# def p_expression_resta(p):
-#     'expression : expression RESTA term'
-#     p[0] = int(p[1]) - int(p[3])
-#     #generacion de polaca inversa
-#     polaca_inversa.append("-")
-
-
-# #las expresiones pueden ser terminos
-# def p_expression_term(p):
-#     'expression : term'
-#     p[0] = p[1]
-
-# #para la multiplicacion de variables/constantes (2 * 3)
-# def p_term_mult(p):
-#     'term : term MULT factor'
-#     p[0] = int(p[1]) * int(p[3])
-#     #generacion de polaca inversa
-#     polaca_inversa.append("*")
-    
-# #para la division de variables/constantes (2 / 3)
-# def p_term_div(p):
-#     'term : term DIV factor'
-#     p[0] = int(p[1]) / int(p[3])
-#     #generacion de polaca inversa
-#     polaca_inversa.append("/")
-
-
-# #un termino puede ser un factor
-# def p_term_factor(p):
-#     'term : factor'
-#     p[0] = p[1]
-
-# #un factor puede ser una constante
-# def p_factor_cte(p):
-#     'factor : CTE'
-#     p[0] = p[1]
-#     #generacion de polaca inversa
-#     polaca_inversa.append(p[1])
-    
-
-# def p_factor_cte_n(p):
-#     "factor : RESTA CTE %prec URESTA"
-#     p[0] = -p[2]
-
-# #permite expresiones como por ej: -(-5 * 8)
-# def p_factor_expr_n(p):
-#     'factor : RESTA PARA expression PARC %prec URESTA'
-#     p[0] = -p[3]    
-
-# #un factor puede ser una variable
-# def p_factor_id(p):
-#     'factor : ID'
-#     valorSimbolo = (lexer.ts.getSymbolByID(p[1])["value"]).iloc[0]
-#     #print(valorSimbolo)
-#     if (lexer.ts.getSymbolByID(p[1])).empty == False: 
-#         p[0] = valorSimbolo #asigno su valor
-#         #generacion de polaca inversa
-#         polaca_inversa.append(p[1])
-#     else:
-#         print("[ERR] Variable '%s' no inicializada" % p[1]) #descripcion del error
-#         p[0] = 0 #para evitar error a nivel lenguaje
-#         raise SyntaxError #hago generar un error de parsing intencional en el parser.
-#     #print("[DEBUG_]: p[0]: ", p[0], " p[1]: ", p[1], " valorSimbolo: ", valorSimbolo)
-
-# #un factor puede ser una expresion encerrada entre parentesis ('(2*3)')
-# def p_factor_expr(p):
-#     'factor : PARA expression PARC'
-#     p[0] = p[2]
-
+#regla lambda
+def p_empty(p):
+    'empty :'
+    pass
 
     
 # Error rule for syntax errors
 def p_error(p):
     if p:
-        print("Syntax error at '%s'" % p.value)
+        e = "[ERR] Error de sintaxis en '"+ p.value + "'"
     else:
-        print("Syntax error at EOF")
+        e = "[ERR] Error de sintaxis en EOF"
+    err.append(e)
+    print(e)
+
 
 
 
@@ -342,5 +316,16 @@ def p_error(p):
 yacc.yacc(write_tables=False)
 yacc.parse(lexer=lexer, debug=False)
 
+#impresion de resulados por consola
 lexer.ts.__str__() 
 print("polaca_inversa: ", polaca_inversa)
+
+#Grabado de resultados a archivo txt
+file = open(path_output + 'output.txt', "w")
+file.write("Errores/Warnings: " + '\n')
+file.write(str(err) + '\n')
+file.write('\n' + "Tabla de simbolos: " + '\n')
+file.write(str(lexer.ts.table) + '\n')
+file.write('\n'+ "polaca_inversa: " + '\n')
+file.write(str(polaca_inversa) + '\n')
+file.close()
